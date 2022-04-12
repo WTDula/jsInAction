80c4535e8c613e632b1ee8db1484350a083b5b12
WTDula <wtdula@gmail.com>
2022-04-12 11:06:47 -0500

    added index.html, worksheet.js, finished variables section

---
+++ worksheet.js
@@ -0,0 +1,12 @@
+// Days of the week
+
+let dayOfWeek = "Monday"
+console.log(dayOfWeek)
+dayOfWeek = "Friday"
+console.log(`I can't wait for ${dayOfWeek}!`)
+
+//User Input
+
+let animalInput = prompt("What is your favorite animal?")
+let colorInput = prompt("What is your favorite color?")
+console.log(`I've never seen a ${colorInput} ${animalInput}!`)
