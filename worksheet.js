38221402ae9edc9651da1aa035b1159296d68887
WTDula <wtdula@gmail.com>
2022-04-12 13:18:59 -0500

    completed conditionals, loops, functions

--- worksheet.js
+++ worksheet.js
@@ -13,35 +13,76 @@
 // console.log(`I've never seen a ${colorInput} ${animalInput}!`)
 
 
-// Conditionals
-// Meals
+// // Conditionals
+// // Meals
 
-let timeOfDay = "food"
-let mysteryMeal
-if(timeOfDay < 1200){
-    mysteryMeal = "Cereal"
-}
-else if(timeOfDay < 1700){
-    mysteryMeal = "Sandwich"
-}
-else if(timeOfDay >= 1700){
-    mysteryMeal = "Steak"
-}
-console.log(mysteryMeal)
+// let timeOfDay = "food"
+// let mysteryMeal
+// if(timeOfDay < 1200){
+//     mysteryMeal = "Cereal"
+// }
+// else if(timeOfDay < 1700){
+//     mysteryMeal = "Sandwich"
+// }
+// else if(timeOfDay >= 1700){
+//     mysteryMeal = "Steak"
+// }
+// console.log(mysteryMeal)
 
-// Random Number
+// // Random Number
 
-let randomBand = Math.floor(Math.random() * 11)
-console.log(randomBand)
-if(randomBand <= 2){
-    console.log("Beatles")
-}
-else if(randomBand <= 5){
-    console.log("Stones")
-}
-else if(randomBand <= 8){
-    console.log("Floyd")
-}
-else{
-    console.log("Hendrix")
-}
+// let randomBand = Math.floor(Math.random() * 11)
+// console.log(randomBand)
+// if(randomBand <= 2){
+//     console.log("Beatles")
+// }
+// else if(randomBand <= 5){
+//     console.log("Stones")
+// }
+// else if(randomBand <= 8){
+//     console.log("Floyd")
+// }
+// else{
+//     console.log("Hendrix")
+// }
+
+// // Loops
+
+// for(let i = 0; i < 7; i++){
+//     console.log("JavaScript is better than python!")
+// }
+// for(let i = 0; i < 11; i++){
+//     console.log(i)
+// }
+// for(let i = 0; i <= 4; i++){
+//     console.log("hello\ngoodbye")
+// }
+
+// // Functions
+// // Favorite movie
+
+// function printMovieName(){
+//     let myFavoriteMovie = "Robin Hood Prince of Thieves"
+//     console.log(myFavoriteMovie)
+// }
+// printMovieName()
+
+// // Favorite Band
+
+// const enterFavoriteBand = () => {
+//     let userInput = prompt("What is your favorite band?")
+//     return userInput
+// }
+// let userFavoriteBand = enterFavoriteBand()
+// console.log(userFavoriteBand)
+
+// // Concert
+
+// function concertDisplay(musicalAct){
+//     let myStreet = prompt("Please enter the name of the street you live on")
+//     console.log(`It would be great if ${musicalAct} played a show on ${myStreet}!`)
+// }
+// concertDisplay(userFavoriteBand)
+
+// Arrays
+
