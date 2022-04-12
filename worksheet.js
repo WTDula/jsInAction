b4dd37975c4c348a197e7939b7bd8a52b2283b63
WTDula <wtdula@gmail.com>
2022-04-12 11:34:22 -0500

    finished conditionals

--- worksheet.js
+++ worksheet.js
@@ -1,12 +1,47 @@
-// Days of the week
+// // Variables
+// // Days of the week
 
-let dayOfWeek = "Monday"
-console.log(dayOfWeek)
-dayOfWeek = "Friday"
-console.log(`I can't wait for ${dayOfWeek}!`)
+// let dayOfWeek = "Monday"
+// console.log(dayOfWeek)
+// dayOfWeek = "Friday"
+// console.log(`I can't wait for ${dayOfWeek}!`)
 
-//User Input
+// // User Input
 
-let animalInput = prompt("What is your favorite animal?")
-let colorInput = prompt("What is your favorite color?")
-console.log(`I've never seen a ${colorInput} ${animalInput}!`)
+// let animalInput = prompt("What is your favorite animal?")
+// let colorInput = prompt("What is your favorite color?")
+// console.log(`I've never seen a ${colorInput} ${animalInput}!`)
+
+
+// Conditionals
+// Meals
+
+let timeOfDay = "food"
+let mysteryMeal
+if(timeOfDay < 1200){
+    mysteryMeal = "Cereal"
+}
+else if(timeOfDay < 1700){
+    mysteryMeal = "Sandwich"
+}
+else if(timeOfDay >= 1700){
+    mysteryMeal = "Steak"
+}
+console.log(mysteryMeal)
+
+// Random Number
+
+let randomBand = Math.floor(Math.random() * 11)
+console.log(randomBand)
+if(randomBand <= 2){
+    console.log("Beatles")
+}
+else if(randomBand <= 5){
+    console.log("Stones")
+}
+else if(randomBand <= 8){
+    console.log("Floyd")
+}
+else{
+    console.log("Hendrix")
+}
