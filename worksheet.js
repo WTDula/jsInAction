748b47c70cee54bb9bfcaa649989029e4d907b8a
WTDula <wtdula@gmail.com>
2022-04-12 13:47:04 -0500

    finished & tested Magic number, uncommented rest of worksheet so it may be viewed all at once

--- worksheet.js
+++ worksheet.js
@@ -1,88 +1,88 @@
-// // Variables
-// // Days of the week
+// Variables
+// Days of the week
 
-// let dayOfWeek = "Monday"
-// console.log(dayOfWeek)
-// dayOfWeek = "Friday"
-// console.log(`I can't wait for ${dayOfWeek}!`)
+let dayOfWeek = "Monday"
+console.log(dayOfWeek)
+dayOfWeek = "Friday"
+console.log(`I can't wait for ${dayOfWeek}!`)
 
-// // User Input
+// User Input
 
-// let animalInput = prompt("What is your favorite animal?")
-// let colorInput = prompt("What is your favorite color?")
-// console.log(`I've never seen a ${colorInput} ${animalInput}!`)
+let animalInput = prompt("What is your favorite animal?")
+let colorInput = prompt("What is your favorite color?")
+console.log(`I've never seen a ${colorInput} ${animalInput}!`)
 
 
-// // Conditionals
-// // Meals
+// Conditionals
+// Meals
 
-// let timeOfDay = "food"
-// let mysteryMeal
-// if(timeOfDay < 1200){
-//     mysteryMeal = "Cereal"
-// }
-// else if(timeOfDay < 1700){
-//     mysteryMeal = "Sandwich"
-// }
-// else if(timeOfDay >= 1700){
-//     mysteryMeal = "Steak"
-// }
-// console.log(mysteryMeal)
+let timeOfDay = 1300
+let mysteryMeal
+if(timeOfDay < 1200){
+    mysteryMeal = "Cereal"
+}
+else if(timeOfDay < 1700){
+    mysteryMeal = "Sandwich"
+}
+else if(timeOfDay >= 1700){
+    mysteryMeal = "Steak"
+}
+console.log(mysteryMeal)
 
-// // Random Number
+// Random Number
 
-// let randomBand = Math.floor(Math.random() * 11)
-// console.log(randomBand)
-// if(randomBand <= 2){
-//     console.log("Beatles")
-// }
-// else if(randomBand <= 5){
-//     console.log("Stones")
-// }
-// else if(randomBand <= 8){
-//     console.log("Floyd")
-// }
-// else{
-//     console.log("Hendrix")
-// }
+let randomBand = Math.floor(Math.random() * 11)
+console.log(randomBand)
+if(randomBand <= 2){
+    console.log("Beatles")
+}
+else if(randomBand <= 5){
+    console.log("Stones")
+}
+else if(randomBand <= 8){
+    console.log("Floyd")
+}
+else{
+    console.log("Hendrix")
+}
 
-// // Loops
+// Loops
 
-// for(let i = 0; i < 7; i++){
-//     console.log("JavaScript is better than python!")
-// }
-// for(let i = 0; i < 11; i++){
-//     console.log(i)
-// }
-// for(let i = 0; i <= 4; i++){
-//     console.log("hello\ngoodbye")
-// }
+for(let i = 0; i < 7; i++){
+    console.log("JavaScript is better than python!")
+}
+for(let i = 0; i < 11; i++){
+    console.log(i)
+}
+for(let i = 0; i <= 4; i++){
+    console.log("hello\ngoodbye")
+}
 
-// // Functions
-// // Favorite movie
+// Functions
+// Favorite movie
 
-// function printMovieName(){
-//     let myFavoriteMovie = "Robin Hood Prince of Thieves"
-//     console.log(myFavoriteMovie)
-// }
-// printMovieName()
+function printMovieName(){
+    let myFavoriteMovie = "Robin Hood Prince of Thieves"
+    console.log(myFavoriteMovie)
+}
+printMovieName()
 
-// // Favorite Band
+// Favorite Band
 
-// const enterFavoriteBand = () => {
-//     let userInput = prompt("What is your favorite band?")
-//     return userInput
-// }
-// let userFavoriteBand = enterFavoriteBand()
-// console.log(userFavoriteBand)
+const enterFavoriteBand = () => {
+    let userInput = prompt("What is your favorite band?")
+    return userInput
+}
+let userFavoriteBand = enterFavoriteBand()
+console.log(userFavoriteBand)
 
-// // Concert
+// Concert
 
-// function concertDisplay(musicalAct){
-//     let myStreet = prompt("Please enter the name of the street you live on")
-//     console.log(`It would be great if ${musicalAct} played a show on ${myStreet}!`)
-// }
-// concertDisplay(userFavoriteBand)
+function concertDisplay(musicalAct){
+    let myStreet = prompt("Please enter the name of the street you live on")
+    console.log(`It would be great if ${musicalAct} played a show on ${myStreet}!`)
+}
+concertDisplay(userFavoriteBand)
 
 // Arrays
 
@@ -91,4 +91,26 @@ console.log(desktopItems[1])
 desktopItems.push("Infinity Gauntlet")
 for(let i = 0; i < desktopItems.length; i++){
     console.log(desktopItems[i])
+}
+
+// Magic Number
+
+let magicNumber = Math.floor(Math.random() * 101)
+let guess = 0
+while(guess != magicNumber){
+    guess = prompt("Enter your new guess")
+    console.log(`Your guess was ${guess}`)
+    if(guess < magicNumber){
+        console.log("Too Low!")
+    }
+    if(guess > magicNumber){
+        console.log("Too High!")
+    }
+    if(guess == magicNumber){
+        console.log(`The magic number was ${magicNumber}.  Congrats, you guessed the number!!`)
+        break
+    }
+    if(Math.abs(guess - magicNumber) <= 10){
+        console.log("Getting warmer!")
+    }
 }
